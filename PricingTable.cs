784fa37cbdadb2d8709b716bdc3662b984d9f038
Alejandro Serrano <trupill@gmail.com>
2014-11-20 16:05:02 +0100

    Initial commit

---
+++ PricingTable.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lab3
+{
+	public static class PricingTable
+	{
+		public static float getPrice(int tariefeenheden, int col)
+		{
+			switch (col) {
+			case 0:
+				return 2.10f;
+			case 1:
+				return 1.70f;
+			case 2:
+				return 1.30f;
+			case 3:
+				return 3.60f;
+			case 4:
+				return 2.90f;
+			case 5:
+				return 2.20f;
+			default:
+				throw new Exception ("Unknown column number");
+			}
+		}
+	}
+}
+
